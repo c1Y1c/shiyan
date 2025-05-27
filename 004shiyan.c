12f03cc11e9d38e9f9e87c1e1ca2eb2b2849ab0d
c1Y1c <1365135112@qq.com>
2025-05-27 11:44:09 +0800

    Update 004shiyan.c

--- 004shiyan.c
+++ 004shiyan.c
@@ -4,33 +4,33 @@ void test01()
 {
     float x;
     scanf("%f", &x);
-    if(x < 1) printf("y=x=%.2f", x);
-    if(x >= 1 && x <= 10) printf("y=2x-1=%.2f", 2 * x - 1);
-    if(x > 10) printf("y=3x-11=%.2f", 3 * x - 11);
+    if(x < 1) printf("y=x=%.2f\n", x);
+    if(x >= 1 && x <= 10) printf("y=2x-1=%.2f\n", 2 * x - 1);
+    if(x > 10) printf("y=3x-11=%.2f\n", 3 * x - 11);
 }
 void test02()
 {
     float a;
     scanf("%f");
-    if(a >= 90) printf("A");
-    if(a >= 80 && a < 89) printf("B");
-    if(a >= 70 && a < 79) printf("C");
-    if(a >= 60 && a < 69) printf("D");
-    if(a < 60) printf("E");
+    if(a >= 90) printf("A\n");
+    if(a >= 80 && a < 89) printf("B\n");
+    if(a >= 70 && a < 79) printf("C\n");
+    if(a >= 60 && a < 69) printf("D\n");
+    if(a < 60) printf("E\n");
 }
 void test03()
 {
     int num, count = 0;
     scanf("%d", &num);
     int a = num;
-    printf("ÄæÐòÊý×Ö:");
+    printf("Ã„Ã¦ÃÃ²ÃŠÃ½Ã—Ã–:");
     while(a != 0)
     {
         printf("%d", a % 10);
         count++;
         a = a / 10;
     }
-    printf("\nÊý×ÖÎ»Êý:%d\n", count);
+    printf("\nÃŠÃ½Ã—Ã–ÃŽÂ»ÃŠÃ½:%d\n", count);
 
 }
 int main()
